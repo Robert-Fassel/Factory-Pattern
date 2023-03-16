fbc653b921ae0c84d90cdd3bfa0ca384bce9e57b
Robert Fassel <fcwrob@gmail.com>
2023-03-15 23:45:43 -0500

    Complete

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numOfWheels;
+            bool input = true;
+            do
+            {
+                Console.Clear();
+                input = true;
+                Console.WriteLine("Please enter the amount of tires for the desired vehicle");
+
+                 input = int.TryParse(Console.ReadLine(), out numOfWheels);
+                if (numOfWheels !=2 && numOfWheels != 3 && numOfWheels != 4)
+                {
+                    input = false;
+                    Console.WriteLine("User input not recognized");
+                    Thread.Sleep(1000);
+                }
+            } while (input == false);
+            Console.Clear();
+
+           var vehicle = VehicleFactory.GetVehicle(numOfWheels);
+            vehicle.Drive();
         }
     }
 }
